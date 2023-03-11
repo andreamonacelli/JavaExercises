2f5ae0961786afd04862aa123e04743e06f49949
andreamonacelli <andreamonacelli02@gmail.com>
2023-03-11 09:54:57 +0100

    10th March 2023 commit

--- src/test/java/com/andreamonacelli/exercises/oop/basic/PolygonTest.java
+++ src/test/java/com/andreamonacelli/exercises/oop/basic/PolygonTest.java
@@ -11,8 +11,8 @@ class PolygonTest {
     @Test
     void getVerticesCount() {
         assertEquals(3, new Polygon(new Point[]{new Point(0,0),new Point(0,3), new Point(3,2)}).getVerticesCount());
-        assertEquals(3, new Polygon(new Point[]{new Point(0,0)}).getVerticesCount());
-        assertEquals(3, new Polygon(new Point[]{new Point(0,0),new Point(0,3)}).getVerticesCount());
+        assertEquals(1, new Polygon(new Point[]{new Point(0,0)}).getVerticesCount());
+        assertEquals(2, new Polygon(new Point[]{new Point(0,0),new Point(0,3)}).getVerticesCount());
     }
 
     @Test
